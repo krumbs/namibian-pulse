84a8f73a1635c0df18d78e0b149fde005688b67a
krumbs <stephaniecjonkers@outlook.com>
2021-07-06 16:38:53 +0200

    Updates requirments.txt with venv pip dependancies. Adds two basic pages

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from datetime import datetime
+import re
 
 
 app = Flask(__name__)
@@ -6,3 +8,18 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello Namibia!"
+
+@app.route("/hello/<name>")
+def welcome_user(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d, %B, %Y at %X")
+
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Welcome " + clean_name + ". It's " + formatted_now
+    return content
